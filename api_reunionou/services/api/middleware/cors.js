686ec376f77956167c89d8f81e024cd8c44782c1
leopold <leopoldln55@gmail.com>
2022-03-24 15:49:51 +0100

    API_Gateway_Mobile fait, non testé

--- api_reunionou/services/api/middleware/cors.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(req, res, next){
-    res.set({
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, authorization',
-        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS'
-    })
-    next();
-}
