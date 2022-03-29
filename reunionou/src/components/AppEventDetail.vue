2d503a67bf70fa19cc8062b11c259e3b8631b339
Valentin <valentinaubertin@outlook.fr>
2022-03-29 11:10:29 +0200

    feat : add messages in event details page

--- reunionou/src/components/AppEventDetail.vue
+++ reunionou/src/components/AppEventDetail.vue
@@ -1,13 +1,25 @@
 <template>
-<div>
-  <main class="container">
-    <h1>Evénement: {{event.titre}}</h1>
-    <h6>{{event.description}}</h6>
-    <div class="">
-      <p><span class="fw-bold">date :</span> {{event.date_RV}}</p>
-      <p><span class="fw-bold">Géolocalisation :</span> {{event.geoloc}}</p>
-    </div>
-  </main>
+  <div>
+    <main class="container">
+      <h1>Evénement: {{event.titre}}</h1>
+      <h6>{{event.description}}</h6>
+      <div class="">
+        <p><span class="fw-bold">date :</span> {{event.date_RV}}</p>
+        <p><span class="fw-bold">Géolocalisation :</span> {{event.geoloc}}</p>
+      </div>
+    </main>
+    <aside class="container d-flex flex-row justify-content-between">
+      <div class="border-top mt-5">
+        <h2>Messages</h2>
+        <article v-for='message in messages' :key='message._id'>
+          <h6><span class="badge bg-primary">{{message.id_createur}}</span></h6>
+          {{message.message}} <span class="badge bg-secondary">{{message.date}}</span>
+        </article>
+      </div>
+      <div class="border-top mt-5">
+        <h2>Invitations</h2>
+      </div>
+    </aside>
   </div>
 </template>
 
@@ -20,7 +32,8 @@ export default {
     return {
       event: null,
       token: null,
-      id: null
+      id: null,
+      messages: null
     };
   },
   beforeMount(){
@@ -40,6 +53,21 @@ export default {
         })
         .then((response) => {
           this.event = response.data.event;
+        })
+        .catch((error) => {
+          console.log(error)
+          this.errored = true;
+        });
+
+        axios
+        .get("http://localhost:8081/events/"+this.$route.params.id+"/messages", {
+          headers: { 
+            'Authorization': `token ${this.token}` 
+          }
+        })
+        .then((response) => {
+          this.messages = response.data;
+          console.log(this.messages);
         })
         .catch((error) => {
           console.log(error)
